85d32d8a8851f742ead172c02c878a5bcebb3f9f
tjumyk <tjumyk@gmail.com>
2014-07-25 12:53:45 +1000

    Fix test script

--- test/RunTest.bat
+++ test/RunTest.bat
@@ -2,3 +2,4 @@
 echo // > assets\css\custom_font.css
 del assets\fonts /s /q
 java -jar ..\dist\webfontgen.jar *.ttf -o assets\fonts -i *.html,*htm -x assets,bin -c assets\css\custom_font.css -h -m -d
+start test.html
